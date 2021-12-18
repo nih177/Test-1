681a90e74b0ff3b0378b24e0be4e87b1846cebb6
nih177 <borellinihanth2002@gmail.com>
2021-12-18 13:03:25 +0530

    Product pages

---
+++ src/pages/product-4/product-4.js
@@ -0,0 +1,274 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+// import './product-2.css'
+
+export default function Product4() {
+    return (
+        <>
+            <div className="p-2-section-1">
+                <div className="product-2-content-container">
+                    {/* <Container> */}
+                    <Row className="mt-4">
+                        {/* <div className="h10p"></div> */}
+                        <Col md={6}>
+                            <img src="/images/p-4-1.svg" className="w100p mt-auto mb-auto" />
+                        </Col>
+                        <Col md={6}>
+                            <div className="d-flex flex-column">
+                                <span className="p-headings text-blue">AgriCell</span>
+                                <span>AGRI001</span>
+                                <div className="h20"></div>
+                                <span>AgriCell Dost provides remote connectivity to your</span>
+                                <span>Motor so that you can control it from the comfort</span>
+                                <span>of your home or anywhere with Cellular connectivity.</span>
+                                <div className="h20"></div>
+
+
+
+
+
+
+
+
+
+                                <div className="icon-box-row">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>Single Phase Preventer</h6>
+                                </div>
+                                <div className="icon-box-row">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>Dry Run Cut Off</h6>
+                                </div>
+                                <div className="icon-box-row">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>Overload Protection</h6>
+                                </div>
+                                <div className="icon-box-row">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>SMS based ON / OFF</h6>
+                                </div>
+                                <div className="icon-box-row">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>Timer Slots</h6>
+                                </div>
+                                <div className="h20"></div>
+                                {/* <div className="d-flex flex-row">
+                                    <div className="d-block">
+                                        Capacity<br />
+                                        <button className="mt-1 bold">3.3 to 7.2kw</button>
+                                    </div>
+                                    <div className="w20"></div>
+                                    <div className="d-block">
+                                        Color<br />
+                                        <button className="mt-1 bold">Deep Blue + White</button>
+                                    </div>
+                                </div> */}
+                                <div className="h20"></div>
+                                <span>Support</span>
+                                <div className="h20"></div>
+                                <div className="d-flex flex-row">
+                                    <button className="bold">Get Price Quotation</button>
+                                    <div className="w20"></div>
+                                    <button className="bold">Download Brocher</button>
+                                </div>
+                                <div className="mt-3"></div>
+                                <div className="d-flex flex-row">
+                                    <button className="bold">Manufacture information</button>
+                                    <div className="w20"></div>
+                                    <button className="bold">Warranty Policy</button>
+                                </div>
+
+                            </div>
+                        </Col>
+                    </Row>
+                    {/* </Container> */}
+                </div>
+            </div>
+            <div className="p-2-section-2">
+                <img src='/images/sec-2.svg' height="300px" width="90%" className="" />
+            </div>
+            <div className="p-2-section-3 pt-4 pb-4">
+                <div className="h20"></div>
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                    <span className="p-headings">Connect, Charge, Go</span>
+
+
+                    <span className="w60p d-flex flex-column justify-content-center align-items-center">To meet the massive EV adoption plans of the government of India, Charging Infrastructure plays a key role.</span>
+                    <span className="w60p d-flex flex-column justify-content-center align-items-center">We are working on modular and robust solutions to support various standards in the EV industry.</span>
+                </div>
+                <div className="h35"></div>
+                <div className="h35"></div>
+                <Row className="w99p">
+                    <Col md={2}></Col>
+                    <Col md={4}>
+                        <div className="h20p"></div>
+                        <div className="d-block mx-auto w100p">
+                            <div className="d-flex flex-column">
+                                <span className="p-headings w283">Smart</span>
+                                <span className="p-headings w283 line-height-0">Protections</span>
+                            </div>
+                            <div className="ml-4">
+                                <div className="h20"></div>
+                                <div className="h10"></div>
+                                <div className="icon-box-row  ">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>Single Phase Preventer</h6>
+                                </div>
+                                <div className="icon-box-row ">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>Timer Slots</h6>
+                                </div>
+                                <div className="icon-box-row ">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>Overload Protection</h6>
+                                </div>
+                            </div>
+                        </div>
+
+                    </Col>
+                    <Col className="" md={6}>
+                        <img src="/images/p-4-2.svg" className="w99p" height="580" />
+                    </Col>
+                </Row>
+                <Row className="w99p">
+                    <Col md={1}></Col>
+                    <Col className="d-flex justify-content-center" md={5}>
+                        <img src="/images/p-4-3.svg" className="w99p" height="580" />
+                    </Col>
+                    <Col md={6}>
+                        <div className="h20p"></div>
+                        <div className="h10p"></div>
+                        <div className="d-block mt-auto mb-auto pl15p">
+                            <span className="p-headings ">Controls</span>
+                            <div className="h10"></div>
+                            <div className="">
+                                7 Inch FHD Touch Display With Simple User Interface
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="w99p">
+                    <Col md={2}></Col>
+                    <Col md={4} className="">
+                        <div className="h20p"></div>
+                        <div className="h10p"></div>
+                        <div className="d-block mx-auto w100p">
+                            <div className="mb-2">
+                                <span className="p-headings w283">Smart</span><br />
+                                <span className="p-headings ">Payment Options</span>
+                            </div>
+                            <div className="ml-4">
+                                <span>CMS Communication & App-based Payment</span>
+                                <div className="h10"></div>
+                                <div className="h5px"></div>
+                                <div className="icon-box-row  ">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>Smart Card Reader</h6>
+                                </div>
+                                <div className="icon-box-row ">
+                                    <img src="/images/feature.png" className="p-2feature-icon" />
+                                    &nbsp;
+                                    <h6>App-based Payment gateway</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col className="" md={6}>
+                        <img src="/images/p-4-4.svg" className="w99p" height="580" />
+                    </Col>
+                </Row>
+
+                <Row className="w99p">
+                    <Col md={1}></Col>
+                    <Col className="" md={5}>
+                        <img src="/images/p-4-1.svg" className="w90p" height="580" />
+                    </Col>
+                    <Col md={6}>
+                        {/* <div className="h20p"></div> */}
+                        <div className="h10p"></div>
+                        <div className="d-block mt-auto mb-auto pl15p">
+                            <span className="p-headings ">Technical Specifications</span><br />
+                            <span className=" ">ATOM DC CHARGER 35KW</span>
+                            <div className="h20"></div>
+                            <div className="d-block">
+                                <table class="table table-borderless w90p">
+                                    <thead>
+                                        <tr className="bg-dark-blue text-white">
+                                            <th scope="col">Specification</th>
+                                            <th scope="col">Description</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Mains Topology</td>
+                                            <td>3 Phase - 3 Wire ( 360 V to 488 V )W</td>
+                                        </tr>
+
+
+
+                                        <tr className="border-black">
+                                            <td>Types Of Starters</td>
+                                            <td>DOL ( Upto 7.5 HP ) , Star Delta<br />( 7.5 HP to 15 HP )</td>
+                                        </tr>
+
+
+                                        <tr>
+                                            <td>Current Range</td>
+                                            <td>1 to 22 Amps, configurable as per<br />pump horse power.</td>
+                                        </tr>
+
+
+                                        <tr className="border-black">
+                                            <td>Sim Cards Supported</td>
+                                            <td>2G ( GSM, GPRS )<br />Temperature</td>
+                                        </tr>
+                                        <tr>
+
+
+
+                                            <td>Protection Degree</td>
+                                            <td>IP54, IP65</td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                {/* <div className="h25"></div> */}
+                {/* <div className="h25"></div> */}
+            </div>
+            <div className="p-2-section-2">
+                <img src='/images/sec-3.svg' height="300px" width="90%" className="" />
+            </div>
+            <div className="p-2-section-4">
+                <div className="h25"></div>
+                <Row>
+                    <Col className="d-flex flex-column justify-content-start align-items-center w50p ">
+                        {/* <div> */}
+                        <img className="w40p mt-1" src="/images/download.svg"></img><div className="h10"></div>
+                        <span className="mt-2">Download</span><div className="h10"></div>
+                        <button className="w200">Brochere User Manual</button>
+                        {/* </div> */}
+                    </Col>
+                    <Col className="d-flex flex-column justify-content-start align-items-center w50p ml50">
+                        <img className="w40p" src="/images/headphones.svg"></img><div className="h10"></div>
+                        <span>Place an Order</span><div className="h10"></div>
+                        <button className="w200">Get in Touch</button>
+                    </Col>
+                </Row>
+                <div className="h25"></div>
+            </div>
+        </>
+    );
+}
